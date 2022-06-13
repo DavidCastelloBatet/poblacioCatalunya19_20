d4b2a7bbeff370248823015420c718e5d28f76bc
David <davidcastellobatet@gmail.com>
2022-06-14 00:16:22 +0200

    paginacio

--- index.php
+++ index.php
@@ -3,16 +3,11 @@
 include 'conexioBBDD.php';
 include 'utilitats.php';
 
-$sql = "SELECT  id,
-                any,
-                nom_poblacio,
-                codi_poblacio
+$sql = "SELECT  *
         FROM poblacio_cat_edat_sexe
-        WHERE id > 0 AND any = 2020
-        LIMIT 10; ";
+        limit 50; ";
 
 $statment = $pdo->query($sql);
-
 // Es fa servir per retornar un element
 //$poblacions = $statment->fetch();
 
@@ -20,11 +15,43 @@ $statment = $pdo->query($sql);
 $poblacions = $statment->fetchAll();
 
 
+
+// LOGICA PAGINACIO
+// Quantitat de poblacions mostrades x pagina
+$poblacionsPerPagina = 10; 
+// averigo el total de poblacions que em retorna la query
+$totalPoblacions = $statment->rowCount();
+// calculo les pàgines i redondejo cap amunt
+$pagines = ceil($totalPoblacions / $poblacionsPerPagina);
+// faig que al carregar sempre hi hagi una pagina activa
+if(!$_GET){
+        header('Location:index.php?pagina=1');
+}
+// si es posa al navegador una pagina que no existeix, redirigim...
+if($_GET['pagina'] > $poblacionsPerPagina || $_GET['pagina'] <= 0) {
+        header('Location:index.php?pagina=1');
+}
+// calcul de la variable pel canvi de pàgina  !!ull tornar a revisasr !!!
+$inici = ($_GET['pagina'] - 1) * $poblacionsPerPagina;
+// https://www.youtube.com/watch?v=tRUg2fSLRJo
+$sql_poblacions = 'SELECT * FROM poblacio_cat_edat_sexe LIMIT :inici , :poblacionsPerPagina ';
+$statment_poblacions = $pdo->prepare($sql_poblacions);
+$statment_poblacions->bindParam(':inici', $inici, PDO::PARAM_INT);
+$statment_poblacions->bindParam(':poblacionsPerPagina', $poblacionsPerPagina, PDO::PARAM_INT);
+$statment_poblacions->execute();
+
+$resultat_poblacions = $statment_poblacions->fetchAll();
+
+
+
+// VISUALITZACIO A LA PAGINA
 include 'header.php';
 
 (!$poblacions) ? include 'informacio-no-trobada.php' : include 'paginaPrincipal.php';
 
 include 'footer.php';
+
+
 
 // desvincular conexio dades i destruir objecte PDO
 disconnect ( $pdo, $statment );
@@ -35,6 +62,8 @@ disconnect ( $pdo, $statment );
 // Preguntar Xavi com saber si la conexio esta tancada....  FET!
 
 // Exemples paginacio:
+
+// Escollit: https://www.youtube.com/watch?v=d8odPbC8Yhk
 
 // https://parzibyte.me/blog/2020/06/24/paginacion-php-mysql/
 
--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -8,7 +8,7 @@
       <th>CODI</th>
       <th>+ Info</th>
     </tr>
-    <?php foreach($poblacions as $poblacio) { ?>
+    <?php foreach($resultat_poblacions as $poblacio) { ?>
         <tr>
 
           <td> <?= html_escape($poblacio['id']) ?> </td>
@@ -21,7 +21,40 @@
       <?php }  ?>
 
   </table>
+  
+  <!-- Estructura i css paginacio creada amb bootstrap 5-->
+  <nav>
+    <ul class="pagination">
 
+      <li class="page-item <?= $_GET['pagina'] <= 1 ? 'disabled' : ''  ?> ">
+        <a class="page-link" 
+        href="index.php?pagina=<?=  $_GET['pagina'] - 1  ?>" >
+        Anterior
+        </a>
+      </li>
+
+      <!-- Amb aquest for creo l'estructura de links a partir de la
+      variable $pagines -->
+      <?php for($i = 0; $i < $pagines; $i++) { ?>
+      <li class="page-item <?= $_GET['pagina'] == $i + 1 ? 'active' : ''  ?> " >
+        <a class="page-link" 
+        href="index.php?pagina=<?= $i + 1  ?>"> 
+        <?= $i + 1  ?>  
+        </a>
+      </li>
+      <?php  }  ?>
+
+      <li class="page-item <?= $_GET['pagina'] >= $pagines ? 'disabled' : ''  ?> ">
+        <a class="page-link" 
+        href="index.php?pagina=<?=  $_GET['pagina']+1  ?>" >
+        Següent
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+  
+  
 </div>
 
 
